1a7306f55b3a7c89f6a43bbb0ab3a58d8716023b
Iury <iurygfranca@gmail.com>
2023-01-25 22:33:39 -0400

    build: modifiers to build vercel #2 🛠

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@ import { siteConfig } from '@/config/site'
 
 export default function Head() {
   const url = process.env.NEXT_PUBLIC_APP_URL
-  const ogUrl = new URL(`${url}/og.jpg`)
+  // const ogUrl = new URL(`${url}/og.jpg`)
 
   return (
     <>
@@ -17,12 +17,12 @@ export default function Head() {
       <meta property='og:title' content={siteConfig.name} />
       <meta property='og:description' content={siteConfig.description} />
       <meta property='og:url' content={url?.toString()} />
-      <meta property='og:image' content={ogUrl.toString()} />
+      {/* <meta property='og:image' content={ogUrl.toString()} /> */}
       <meta name='twitter:title' content={siteConfig.name} />
       <meta name='twitter:description' content={siteConfig.description} />
       <meta name='twitter:card' content='summary_large_image' />
       <meta property='twitter:url' content={url?.toString()} />
-      <meta name='twitter:image' content={ogUrl.toString()} />
+      {/* <meta name='twitter:image' content={ogUrl.toString()} /> */}
     </>
   )
 }
