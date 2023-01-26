c7b5dccd562e4bb26ec5d6307dd6b61db6b45d4c
Iury <iurygfranca@gmail.com>
2023-01-25 22:24:00 -0400

    build: modifiers to build vercel🛠

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
-const { withContentlayer } = require("next-contentlayer");
+// const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
+    domains: ['avatars.githubusercontent.com', 'images.unsplash.com'],
   },
   experimental: {
     appDir: true,
     fontLoaders: [
       {
-        loader: "@next/font/google",
-        options: { subsets: ["latin"] },
+        loader: '@next/font/google',
+        options: { subsets: ['latin'] },
       },
     ],
   },
-};
+}
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig
