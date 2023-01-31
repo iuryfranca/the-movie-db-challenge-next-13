88395ac83a173a254b5f1b6591abe427ecbf8f12
Iury <iurygfranca@gmail.com>
2023-01-31 11:40:44 -0400

    style: changing logo and small settings in mobile header💎

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import logoShort from '../../public/blue_short.svg'
+
 import Link from 'next/link'
 import Image from 'next/image'
+import logoBlueSquare from '../../public/blue_square_2.svg'
 
 import { cn } from '@/lib/utils'
 import { useLockBody } from '@/hooks/use-lock-body'
@@ -32,18 +33,17 @@ export function MobileNav() {
         'fixed inset-0 top-14 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 animate-in slide-in-from-bottom-80 md:hidden'
       )}
     >
-      <div className='z-20grid relative gap-6 rounded-md border-2 border-slate-300 bg-white p-4 antialiased shadow-lg dark:border-slate-700 dark:bg-slate-800'>
+      <div className='z-20grid  relative flex flex-col gap-6 rounded-md border-2 border-slate-300 bg-white p-4 antialiased shadow-lg dark:border-slate-700 dark:bg-slate-800'>
         <Link href='/' className='flex items-center space-x-2'>
           <Image
-            src={logoShort?.src}
+            src={logoBlueSquare?.src}
             alt='Logo Short'
-            height={logoShort?.height}
-            width={logoShort?.width / 2}
+            height={logoBlueSquare?.height / 5}
+            width={logoBlueSquare?.width / 5}
           />
           <span className='font-bold'>{siteConfig.name}</span>
         </Link>
-        {/* <nav className='grid grid-flow-row auto-rows-max text-sm'> */}
-        {/* <Menubar className='rounded-none border-b border-none dark:bg-slate-900'> */}
+
         <Menubar className='className="grid text-sm" grid-flow-row auto-rows-max'>
           <MenubarMenu>
             <MenubarTrigger className='font-bold'>Filmes</MenubarTrigger>
@@ -136,7 +136,6 @@ export function MobileNav() {
             </MenubarContent>
           </MenubarMenu>
         </Menubar>
-        {/* </nav> */}
       </div>
     </div>
   )
