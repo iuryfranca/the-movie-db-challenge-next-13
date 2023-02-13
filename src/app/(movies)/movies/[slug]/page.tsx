f4e922524debc52653b2f7785f228b3f7daf3740
Iury <iurygfranca@gmail.com>
2023-02-13 19:31:40 -0400

    feat: adding responsiveness  new page Details Movie ✨

--- src/app/(movies)/movies/[slug]/page.tsx
+++ src/app/(movies)/movies/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default function MoviePage({ params }: MoviePageProps) {
       {loadingData ? (
         <div>Carregando...</div>
       ) : (
-        <div className='relative h-[510px] w-full bg-cover bg-no-repeat'>
+        <div className='relative h-[400px] w-full bg-cover bg-no-repeat sm:h-[500px]'>
           <Image
             src={srcBackdropPath}
             alt='Background Image Movie Details'
@@ -36,16 +36,18 @@ export default function MoviePage({ params }: MoviePageProps) {
             className='-z-10 blur-sm brightness-50'
             fill
           />
-          <div className='z-10 grid h-full w-full grid-cols-[300px_1fr] py-10 px-7 text-white'>
-            <Image
-              src={srcPosterPath}
-              alt='Image Movie Poster'
-              className='self-center justify-self-center rounded-lg shadow-lg'
-              width={200}
-              height={350}
-              quality={100}
-            />
-            <section className='flex flex-wrap content-center items-start'>
+          <div className='z-10 grid h-full w-full grid-rows-[400px_1fr] text-white sm:grid-cols-[300px_1fr] sm:py-10 sm:px-7'>
+            <div className='flex h-full w-full items-center justify-center'>
+              <Image
+                src={srcPosterPath}
+                alt='Image Movie Poster'
+                className='rounded-lg shadow-lg'
+                width={200}
+                height={350}
+                quality={100}
+              />
+            </div>
+            <section className='flex flex-wrap content-center items-start py-10 px-7'>
               <div className='mb-6 flex w-full flex-col flex-wrap'>
                 <h1 className='text-3xl font-semibold'>
                   {detailsMovie?.title}
